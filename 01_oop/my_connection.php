997790a3eecbc5fc6bad9318c89c56f95a900dd3
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-03-18 11:30:19 -0700

    Codificar métodos getById y similares
    Se realizó la codificación de los métodos getById, getByName, getByEmail

--- 01_oop/my_connection.php
+++ 01_oop/my_connection.php
@@ -1,7 +1,8 @@
 <?php
+// nombres de clase en formato StudlyCaps
 class MyConnection extends mysqli {
     public function __construct() {
-        $conn = parent::__construct('127.0.0.1','demo','123','demo',3306);
+        $conn = parent::__construct('127.0.0.1','demo','123','demo',3308);
         if ($conn->connect_error) {
             die('Error de conexión ('.$conn->connect_errno.') '.$conn->connect_error);
         }
--- 01_oop/user.php
+++ 01_oop/user.php
@@ -32,28 +32,92 @@ class User {
     }
 
     public function __set($name, $value) {
-        if ($name == 'id') {
-            $this->id = $value;
-        }
-
         if (array_key_exists($name, $this->fields)) {
             $this->fields[$name] = $value;
         }
     }
 
     public static function getById($id) {
-        // TODO: definir la consulta
-        // TODO: abrir la conexion a la base de datos
-        // TODO: ejecutar la consulta
-        // TODO: evaluar el resultado
-        // TODO: retornar null para cuando no se encuentra o un objeto tipo User
+        // Done: definir la consulta
+        $sql = sprintf("select id,name,email,firstname,lastname from users where id=%d", $id);
+        // Done: abrir la conexion a la base de datos
+        $conn = new MyConnection();
+        // Done: ejecutar la consulta
+        $rst = $conn->query($sql);
+        // Forgotten: Cerrar la conexión
+        $conn->close();
+        // Done: evaluar el resultado
+        /**
+         * mysqli_query()
+         * Retorna FALSE en caso de error. Si una consulta del tipo SELECT,
+         * SHOW, DESCRIBE o EXPLAIN es exitosa, mysqli_query() retornará un
+         * objeto mysqli_result. Para otras consultas exitosas de
+         * mysqli_query() retornará TRUE. 
+         */
+        if (!$rst) {
+            die("Error al ejecutar la consulta: $sql");
+        } elseif ($rst->num_rows == 1) {
+            // encontro el registro
+            $user = new User();
+            // obtener el registro en formato de arreglo asociativo
+            $row = $rst->fetch_assoc();
+            $user->id = $row['id'];
+            $user->name = $row['name'];
+            $user->email = $row['email'];
+            $user->firstname = $row['firstname'];
+            $user->lastname = $row['lastname'];
+            return $user;
+        } else {
+            // no encontro resultados
+            return null;
+        }
+        // Done: retornar null para cuando no se encuentra o un objeto tipo User
     }
 
-    public static function getByName($name) {}
-    public static function getByEmail($email) {}
+    public static function getByName($name) {
+        $sql = sprintf("select id,name,email,firstname,lastname from users where name='%s'", $name);
+        $conn = new MyConnection();
+        $rst = $conn->query($sql);
+        $conn->close();
+        if (!$rst) {
+            die("Error al ejecutar la consulta: $sql");
+        } elseif ($rst->num_rows == 1) {
+            $user = new User();
+            $row = $rst->fetch_assoc();
+            $user->id = $row['id'];
+            $user->name = $row['name'];
+            $user->email = $row['email'];
+            $user->firstname = $row['firstname'];
+            $user->lastname = $row['lastname'];
+            return $user;
+        } else {
+            return null;
+        }
+    }
+    public static function getByEmail($email) {
+        $sql = sprintf("select id,name,email,firstname,lastname from users where email='%s'", $email);
+        $conn = new MyConnection();
+        $rst = $conn->query($sql);
+        $conn->close();
+        if (!$rst) {
+            die("Error al ejecutar la consulta: $sql");
+        } elseif ($rst->num_rows == 1) {
+            $user = new User();
+            $row = $rst->fetch_assoc();
+            $user->id = $row['id'];
+            $user->name = $row['name'];
+            $user->email = $row['email'];
+            $user->firstname = $row['firstname'];
+            $user->lastname = $row['lastname'];
+            return $user;
+        } else {
+            return null;
+        }
+    }
 }
 
-// crear un objeto (instanciar la clase User)
-$usuario = new User();
-$usuario->id = 1;
-echo $usuario->id;
+// código de prueba que se eliminará una vez se concluya esta clase User
+// $resultado = User::getById(1);
+// $resultado = User::getByName('bidkar');
+$resultado = User::getByEmail('bidkar.aragon@udo.mx');
+var_dump($resultado);
